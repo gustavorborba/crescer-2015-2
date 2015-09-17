f9d60731158ffa09e8ae1d9f78ad73925f238879
Gustavo Borba <gustavorobaski@hotmail.com>
2015-09-17 13:55:24 -0300

    Adicionado Testes

--- src/modulo-01-java-OO/Modulo01/ElfoTest.java
+++ src/modulo-01-java-OO/Modulo01/ElfoTest.java
@@ -14,56 +14,81 @@ import org.junit.Test;
 public class ElfoTest
 {
    @Test
-    public void elfoTem42Flechas(){
-		Elfo elrond=new Elfo("Elrond");
-		assertEquals(42,elrond.getFlecha());
-	}
-	@Test
-	public void elfoAtiraFlechaGanhaExperie(){
-		Elfo elrond=new Elfo("Elrond");
-		elrond.atirarFlecha();
-		assertEquals(1,elrond.getExp());
-	}
-	@Test
-	public void elfoPerdeFlechaAoAtirar(){
-		Elfo elrond=new Elfo("Elrond");
-		elrond.atirarFlecha();
-		assertEquals(41,elrond.getFlecha());
-	}
-	@Test
-	public void elfoIniciaComFlecha(){
-	    Elfo elrond=new Elfo("Elrond",84);
-	    assertEquals(84,elrond.getFlecha());
-	}
-	@Test 
-	public void elfoNotDefault(){
-	   Elfo elrond=new Elfo("Elrond");
-	   assertNotSame(50,elrond.getFlecha());
-	}
-	@Test
-	public void elfoNomeValidoAposCriacao(){
-	    Elfo elrond=new Elfo("Elrond");
-	    assertEquals("Elrond",elrond.getNome());
-	    assertNotSame("Planeta",elrond.getNome());
-	}
-	@Test
-	public void ElfoTemToStringCorreto(){
-	    Elfo galadriel=new Elfo("Galadriel");
-	    String fraseTeste;
-	    fraseTeste = galadriel.getNome();
+    public void elfoTem42FlechasPorDefault(){
+        Elfo elrond=new Elfo("Elrond");
+        assertEquals(42,elrond.getFlecha());
+    }
+    
+    @Test
+    public void elfoAtiraFlechaGanhaExperie(){
+        Elfo elrond=new Elfo("Elrond");
+        elrond.atirarFlecha();
+        assertEquals(1,elrond.getExp());
+    }
+    
+    @Test
+    public void elfoPerdeFlechaAoAtirar(){
+        Elfo elrond=new Elfo("Elrond");
+        elrond.atirarFlecha();
+        assertEquals(41,elrond.getFlecha());
+        //perde apenas 1
+        assertNotSame(40,elrond.getFlecha());
+    }
+    
+    @Test
+    public void elfoIniciaComFlecha(){
+        Elfo elrond=new Elfo("Elrond",84);
+        assertEquals(84,elrond.getFlecha());
+    }
+    
+    @Test 
+    public void elfoDefaulte42(){
+       Elfo elrond=new Elfo("Elrond");
+       assertNotSame(50,elrond.getFlecha());
+    }
+    
+    @Test
+    public void elfoNomeValidoAposCriacao(){
+        Elfo elrond=new Elfo("Elrond");
+        assertEquals("Elrond",elrond.getNome());
+        assertNotSame("Planeta",elrond.getNome());
+    }
+    
+        @Test
+    public void ElfoAtacaVariosDwarf(){
+        Elfo malfurion=new Elfo("Malfurion");
+        Dwarf grande=new Dwarf("Grande");
+        Dwarf machado=new Dwarf("Machado");
+        Dwarf assis=new Dwarf("Assis");
+        malfurion.atacDwarf(grande);
+        malfurion.atacDwarf(machado);
+        malfurion.atacDwarf(assis);
+        assertEquals(100,grande.getVida());
+        assertEquals(100,machado.getVida());
+        assertEquals(100,assis.getVida());
+        assertEquals(39,malfurion.getFlecha());
+        
+    }
+    
+    @Test
+    public void ElfoTemToStringCorreto(){
+        Elfo galadriel=new Elfo("Galadriel");
+        String fraseTeste;
+        fraseTeste = galadriel.getNome();
         fraseTeste += " possui ";
         fraseTeste += galadriel.getFlecha();
         fraseTeste += " flechas e ";
         fraseTeste += galadriel.getExp();
         fraseTeste += " niveis de experiencia";
         assertEquals(fraseTeste,galadriel.toString());
-	}
-	@Test
-	public void ElfoTemToStringExtenso(){
-	   Elfo galadriel=new Elfo("Galadriel");
-	   assertEquals("Galadriel possui 42 flechas e 0 niveis de experiencia",galadriel.toString());
-	   galadriel.atirarFlecha();
-	   assertEquals("Galadriel possui 41 flechas e 1 niveis de experiencia",galadriel.toString());
-	   assertNotSame("Galadriel possui 40 flechas e 1 niveis de experiencia",galadriel.toString());
-	}
+    }
+    
+    @Test
+    public void ElfoTemToStringExtenso(){
+       Elfo galadriel=new Elfo("Galadriel");
+       assertEquals("Galadriel possui 42 flechas e 0 niveis de experiencia",galadriel.toString());
+       galadriel.atirarFlecha();
+       assertEquals("Galadriel possui 41 flechas e 1 niveis de experiencia",galadriel.toString());
+       assertNotSame("Galadriel possui 40 flechas e 1 niveis de experiencia",galadriel.toString());
+    }
 }
