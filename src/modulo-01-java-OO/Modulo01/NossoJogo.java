fbb276e55909b5f5c8ebcbb7948f602577835908
Gustavo Borba <gustavorobaski@hotmail.com>
2015-09-24 16:52:01 -0300

    Feature(NossoJogo) incluido  main

---
+++ src/modulo-01-java-OO/Modulo01/NossoJogo.java
@@ -0,0 +1,6 @@
+public class NossoJogo{
+    public final static String NOME="Bahhh of the rings";
+    public static void main(String args[]){
+        System.out.println("Olá, bem vindo ao "+ NOME);
+    }
+}
