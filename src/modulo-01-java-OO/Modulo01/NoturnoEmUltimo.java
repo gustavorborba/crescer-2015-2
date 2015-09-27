631083d8525b4918a14db57131ad9326add6f73c
gustavorborba <gustavorobaski@hotmail.com>
2015-09-26 21:18:41 -0300

    Nova Estrategia NoturnoEmUltimo adicionada e seus respectivos testes

---
+++ src/modulo-01-java-OO/Modulo01/NoturnoEmUltimo.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+class NoturnoEmUltimo extends EstrategiaPreparacao implements EstrategiaDeAtaque{
+    private ArrayList<Elfo> ultimoAtaque=new ArrayList<>();
+    public NoturnoEmUltimo(){}
+    public void atacar(ArrayList<Dwarf> anoesHorda,ArrayList<Elfo> elfosArmy){
+        for(Elfo elfo : elfosArmy){
+            if(eElfoVerde(elfo)){
+                ultimoAtaque.add(elfo);
+                    for(Dwarf anao : anoesHorda){
+                         elfo.atirarFlecha(anao);
+                        }
+                    }
+            }
+        for(Elfo elfo : elfosArmy){
+            if(eElfoNoturno(elfo)){
+                ultimoAtaque.add(elfo);
+                    for(Dwarf anao : anoesHorda){
+                         elfo.atirarFlecha(anao);
+                        }
+                    }
+                }
+    }
+    public ArrayList<Elfo> getUltimoAtaque(){
+        return ultimoAtaque;
+    }
+}
---
+++ src/modulo-01-java-OO/Modulo01/NoturnoEmUltimoTest.java
@@ -0,0 +1,79 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+public class NoturnoEmUltimoTest
+{
+    @Test
+    public void elfoNoturnoPorUltimo(){
+        ExercitoElfico exer=new ExercitoElfico();
+        ElfoVerde elfoV1=new ElfoVerde("V1");
+        ElfoVerde elfoV2=new ElfoVerde("V2");
+        ElfoNoturno elfoN1=new ElfoNoturno("N1");
+        
+        exer.alistarElfo(elfoN1);
+        exer.alistarElfo(elfoV1);
+        exer.alistarElfo(elfoV2);
+        
+        ArrayList<Elfo> esperado=new ArrayList<>();
+        
+        esperado.add(elfoV1);
+        esperado.add(elfoV2);
+        esperado.add(elfoN1);
+        
+        ArrayList<Dwarf> anoes=new ArrayList<>();
+        anoes.add(new Dwarf("AS"));
+        anoes.add(new Dwarf("At"));
+        anoes.add(new Dwarf("Ats"));
+        
+        exer.atacarNoturnoUltimo(anoes);
+        
+        assertEquals(esperado,exer.getUltimoAtaque());
+        
+    }
+    @Test
+    public void elfoNoturnoPorUltimoHardcore(){
+        ExercitoElfico exer=new ExercitoElfico();
+        ElfoVerde elfoV1=new ElfoVerde("V1");
+        ElfoVerde elfoV2=new ElfoVerde("V2");
+        ElfoVerde elfoV3=new ElfoVerde("V3");
+        ElfoVerde elfoV4=new ElfoVerde("V4");
+        ElfoVerde elfoV5=new ElfoVerde("V5");
+        
+        ElfoNoturno elfoN1=new ElfoNoturno("N1");
+        ElfoNoturno elfoN2=new ElfoNoturno("N2");
+        ElfoNoturno elfoN3=new ElfoNoturno("N3");
+        
+        exer.alistarElfo(elfoN1);
+        exer.alistarElfo(elfoV1);
+        exer.alistarElfo(elfoV2);
+        exer.alistarElfo(elfoN2);
+        exer.alistarElfo(elfoV3);
+        exer.alistarElfo(elfoN3);
+        exer.alistarElfo(elfoV4);
+        exer.alistarElfo(elfoV5);
+        
+        ArrayList<Elfo> esperado=new ArrayList<>();
+        esperado.add(elfoV1);
+        esperado.add(elfoV2);
+        esperado.add(elfoV3);
+        esperado.add(elfoV4);
+        esperado.add(elfoV5);
+        esperado.add(elfoN1);
+        esperado.add(elfoN2);
+        esperado.add(elfoN3);
+        
+        ArrayList<Dwarf> anoes=new ArrayList<>();
+        anoes.add(new Dwarf("AS"));
+        anoes.add(new Dwarf("At"));
+        anoes.add(new Dwarf("Ats"));
+        
+        exer.atacarNoturnoUltimo(anoes);
+        assertEquals(esperado,exer.getUltimoAtaque());
+        
+    }
+    
+}
