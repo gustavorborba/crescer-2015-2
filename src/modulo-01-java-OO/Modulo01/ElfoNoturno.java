a0e7dab872451df150da3ef7f747bdcf2612e40c
gustavorborba <gustavorobaski@hotmail.com>
2015-09-27 21:40:19 -0300

    Pode-se definir flechas iniciais

--- src/modulo-01-java-OO/Modulo01/ElfoNoturno.java
+++ src/modulo-01-java-OO/Modulo01/ElfoNoturno.java
@@ -9,13 +9,14 @@ public class ElfoNoturno extends Elfo{
     public ElfoNoturno(String novoNome){
         super(novoNome);
     }
+    public ElfoNoturno(String novoNome,int quantFlecha){
+        super(novoNome,quantFlecha);
+    }
     public void atirarFlecha(Dwarf anao){
         super.atirarFlecha(anao);
         this.experiencia += 2;
         double qtdVidaAPerder = this.vida * 0.05;
-        //double qtdVidaAPerder = this.vida * 5/100;
         this.vida -= qtdVidaAPerder;
-        this.status = (int)this.vida == 0 ? 
-            Status.MORTO : this.status;
+        this.status = (int)this.vida == 0 ? Status.MORTO : this.status;
    }
 }
