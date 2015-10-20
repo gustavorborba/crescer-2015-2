1a973c97866ff025ceaa3e90789aeb64bc871eb1
gustavorborba <gustavorobaski@hotmail.com>
2015-10-20 21:35:52 -0200

    Corrigido erro de envio

--- crescer-2015-2/ExerA2/Acao.java
+++
@@ -1,9 +0,0 @@
-//exercicio3
-public class Acao{
-	public void AtirarFlecha(Elfo flec){
-		if(flec.getflec()>0){
-			flec.exper();
-			flec.sflechas();
-		}
-	}
-}
--- crescer-2015-2/src/modulo-01-java-OO/Modulo01/NossoJogo.java
+++
@@ -1,6 +0,0 @@
-public class NossoJogo{
-    public final static String NOME="Bahhh of the rings";
-    public static void main(String args[]){
-        System.out.println("Olá, bem vindo ao "+ NOME);
-    }
-}
