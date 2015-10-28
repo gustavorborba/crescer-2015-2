d358a083ada4ba1d39d56ea2342b625aee1f5c13
gustavorborba <gustavorobaski@hotmail.com>
2015-10-28 12:07:46 -0200

    Base para projeto Locadora

---
+++ src/modulo-04-C#/Locadora/Locadora.Dominio/Jogo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio
+{
+    public class Jogo
+    {
+        public string ID { get; private set; }
+        public string Nome { get; private set; }
+        public string Preco { get; private set; }
+        public string Categoria { get; private set; }
+
+        public Jogo(string id, string nome, string preco, string categoria)
+        {
+            this.ID = id;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Categoria = categoria;
+        }
+    }
+}
