f98d9b9892d1093ea3970addb0bd2d659d22d13e
gustavorborba <gustavorobaski@hotmail.com>
2015-10-28 00:11:43 -0200

    Adicionado exercicios dia 2.  PS falta testes de algums metodos

---
+++ src/modulo-04-C#/dia-02/DbFuncionarios/DbFuncionarios.testes/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+namespace DbFuncionarios.testes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OrdenadoPorCargo()
+        {
+            Exercicios exer = new Exercicios();
+            var ordenado = exer.OrdenadoPorCargo();
+            Assert.AreNotEqual(ordenado, exer.funcionarios);
+        }
+        [TestMethod]
+        public void BuscaPorNome()
+        {
+            Exercicios exer = new Exercicios();
+            var busca = exer.BuscarPorNome("Lucas Leal").ToList();
+            Assert.AreEqual(busca.Count,1);
+        }
+    }
+}
