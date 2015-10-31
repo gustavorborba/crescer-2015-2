bd5ea10ead1993332ec07e0d2e1aa03eb4f90005
gustavorborba <gustavorobaski@hotmail.com>
2015-10-31 20:18:14 -0200

    Inicio de Testes

---
+++ src/modulo-04-C#/Locadora/Testes.Locadora/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using Locadora.Dominio;
+namespace Testes.Locadora
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Banco banco = new Banco();
+        [TestMethod]
+        public void BuscarPorNome1()
+        {
+            var jogo = banco.BuscarPorNome("halo");
+            Assert.AreEqual(jogo.Nome,"Halo");
+            Assert.AreEqual(jogo.ID,21);          
+        }
+
+    }
+}
