df8be58f7a66778d7fce5f775d7e83bd1badf56e
Gustavo Borba <gustavorobaski@hotmail.com>
2015-11-04 17:34:02 -0200

    Base Locadora 2.0, a ser feito devido a problemas tecnicos

---
+++ src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Controllers/RelatorioController.cs
@@ -0,0 +1,19 @@
+﻿using Locadora.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora.Web.MVC.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        public ActionResult JogosDisponiveis()
+        {
+            
+
+            return View(model);
+        }
+    }
+}
