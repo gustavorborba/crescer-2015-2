6a6b955ddd20d9fc57c17e1a9040037bb525a7c6
gustavorborba <gustavorobaski@hotmail.com>
2015-11-04 22:16:05 -0200

    Refatoring Locadora.WEB em processo

--- src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Controllers/RelatorioController.cs
+++ src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Controllers/RelatorioController.cs
@@ -4,15 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Locadora.Dominio.Repositorio;
+using Locadora.Dominio;
 namespace Locadora.Web.MVC.Controllers
 {
     public class RelatorioController : Controller
     {
         public ActionResult JogosDisponiveis()
         {
-            
+           IJogoRepositorio repositorio = new Repositorio.ADO.JogoRepositorio();
 
+        var jogos = repositorio.BuscarTodos().ToList();
+
+            var model = new RelatorioModel();
+            foreach (var item in jogos)
+            {
+                JogoModel jogo = new JogoModel()
+                {
+                    Nome = item.Nome,
+                    Preco = item.Preco,
+                    Categoria = item.Categoria.ToString()
+
+                };
+                model.ListaJogos.Add(jogo);
+            }
+            decimal barato = model.ListaJogos.Min(it => it.Preco);
+            model.MaisBarato = model.ListaJogos.FirstOrDefault(it => it.Preco == barato).Nome;
+            decimal caro = model.ListaJogos.Max(it => it.Preco);
+            model.MaisCaro = model.ListaJogos.FirstOrDefault(it => it.Preco == caro).Nome;
+            model.TotalJogos = model.ListaJogos.Count;
             return View(model);
         }
     }
---
+++ src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Models/RelatorioModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class RelatorioModel
+    {
+        public List<JogoModel> ListaJogos;
+        public int TotalJogos { get; set; }
+        public string MaisCaro { get; set; }
+        public string MaisBarato { get; set; }
+
+        public RelatorioModel()
+        {
+            ListaJogos = new List<JogoModel>();
+        }
+    }
+}
