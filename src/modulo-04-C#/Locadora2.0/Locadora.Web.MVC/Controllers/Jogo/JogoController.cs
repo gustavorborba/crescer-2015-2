b29b5eaec5839a8ce0a0f3b24b2f27b6598ac4ec
gustavorborba <gustavorobaski@hotmail.com>
2015-11-07 14:54:13 -0200

    Feat -> implementado edição de jogos

--- src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Controllers/Jogo/JogoController.cs
+++ src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Controllers/Jogo/JogoController.cs
@@ -5,20 +5,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Locadora.Dominio;
 namespace Locadora.Web.MVC.Controllers.Jogo
 {
     public class JogoController : Controller
     {
+        IJogoRepositorio repositorio = new Repositorio.ADO.JogoRepositorio();
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Buscar(string nome)
         {
-            IJogoRepositorio repositorio = new Repositorio.ADO.JogoRepositorio();
-            var jogos = repositorio.BuscarPorNome(nome);
 
+            var jogos = repositorio.BuscarPorNome(nome);
 
             return View();
         }
@@ -29,8 +29,8 @@ namespace Locadora.Web.MVC.Controllers.Jogo
 
             DescricaoModel descricao = new DescricaoModel();
             descricao.Nome = jogo.Nome;
-            descricao.Categoria = jogo.Categoria.ToString();
-            descricao.Selo = jogo.Selos.ToString();
+            descricao.Categorias = jogo.Categoria;
+            descricao.Selos = jogo.Selos;
             descricao.Imagem = jogo.ImagemUrl;
             descricao.Video = jogo.VideoUrl;
             descricao.Descricao = jogo.Descricao;
@@ -71,6 +71,50 @@ namespace Locadora.Web.MVC.Controllers.Jogo
             model.MaisCaro = model.ListaJogos.FirstOrDefault(it => it.Preco == caro).Nome;
             model.TotalJogos = model.ListaJogos.Count;
             return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int? id)
+        {
+            if (id.HasValue)
+            {
+                var jogo = repositorio.BuscarPorId(Convert.ToInt32(id));
+                var model = new DescricaoModel();
+                model.Nome = jogo.Nome;
+                model.Categorias = jogo.Categoria;
+                model.Selos = jogo.Selos;
+                model.Descricao = jogo.Descricao;
+                model.Imagem = jogo.ImagemUrl;
+                model.Video = jogo.VideoUrl;
+                model.ID = jogo.Id;
+                model.Preco = jogo.Preco;
+                return View(model);
+            }
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Salvar(DescricaoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var jogo = new Dominio.Jogo(model.ID);
+                jogo.Nome = model.Nome;
+                jogo.Categoria = model.Categorias;
+                jogo.Descricao = model.Descricao;
+                jogo.Selos = model.Selos;
+                jogo.ImagemUrl = model.Imagem;
+                jogo.VideoUrl = model.Video;
+                jogo.Preco = model.Preco;
+                repositorio.Atualizar(jogo);
+
+                TempData["Mensagem"] = "Cliente salvo com sucesso!";
+
+                return RedirectToAction("JogosDisponiveis", "Jogo");
+            }
+
+            return View("Editar",model);
         }
     }
 }
--- src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Models/Jogo/DescricaoModel.cs
+++ src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Models/Jogo/DescricaoModel.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Locadora.Dominio;
+using System.ComponentModel.DataAnnotations;
 
 namespace Locadora.Web.MVC.Models
 {
     public class DescricaoModel 
     {
+        [Required(ErrorMessage = "Campo Nome Obrigatorio")]
+        [StringLength(250, MinimumLength = 5, ErrorMessage = "O nome deve ter entre 5 e 250 caracteres")]
         public string Nome { get; set; }
-        public string Categoria { get; set; }
+
+        [Required]
+        public  Categoria Categorias { get; set; }
+
+        [Required]
         public string Descricao { get; set; }
         public string Imagem { get; set; }
         public string Video { get; set; }
-        public string Selo { get; set; }
+
+        [Required]
+        public Selo Selos { get; set; }
+        public decimal Preco { get; set; }
+        public int ID { get; set; }
     }
 }
