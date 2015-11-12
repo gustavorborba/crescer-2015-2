697a9929036c1d1410521244fc0fc142b80b35b6
gustavorborba <gustavorobaski@hotmail.com>
2015-11-11 23:34:17 -0200

    Feat -> controller e view base para Locacao

---
+++ src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Controllers/Locacao/LocacaoController.cs
@@ -0,0 +1,54 @@
+﻿using Locadora.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Locadora.Repositorio.EF;
+
+namespace Locadora.Web.MVC.Controllers.Locacao
+{
+    public class LocacaoController : Controller
+    {
+        // GET: Locacao
+        public ActionResult LocarPartial(int id)
+        {
+            JogoRepositorio repositorio = new JogoRepositorio();
+            var jogo = repositorio.BuscarPorID(id);
+
+            LocarModel model = new LocarModel();
+            model.Nome = jogo.Nome;
+            model.IDJogo = jogo.IDJogo;
+            model.Imagem = jogo.ImagemUrl;
+            if(jogo.Selos == Dominio.Selo.OURO)
+            {
+                model.DataEntrega = DateTime.Now.AddDays(1);
+                model.Preco = 15;
+            }
+            if(jogo.Selos == Dominio.Selo.PRATA)
+            {
+                model.DataEntrega = DateTime.Now.AddDays(2);
+                model.Preco = 10;
+            }
+            if(jogo.Selos == Dominio.Selo.BRONZE)
+            {
+                model.DataEntrega = DateTime.Now.AddDays(3);
+                model.Preco = 5;
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Locar(int id)
+        {
+
+
+            return View();
+        }
+        public ActionResult SalvarLocacao()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Models/LocarModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class LocarModel
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataEntrega { get; set; }
+        public string Imagem { get; set; }
+        public int IDJogo { get; set; }
+    }
+}
