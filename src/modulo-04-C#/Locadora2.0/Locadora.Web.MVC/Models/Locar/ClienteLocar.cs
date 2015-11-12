d02d8c54249e3c871a2dc0a2dd5f9d6b384af97c
gustavorborba <gustavorobaski@hotmail.com>
2015-11-12 02:38:54 -0200

    Arquivos de Jquery

---
+++ src/modulo-04-C#/Locadora2.0/Locadora.Web.MVC/Models/Locar/ClienteLocar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class ClienteLocar
+    {
+        public int IDJogo { get; set; }
+        [Required(ErrorMessage ="Campo Obrigatorio")]
+        public string Nome { get; set; }
+    }
+}
