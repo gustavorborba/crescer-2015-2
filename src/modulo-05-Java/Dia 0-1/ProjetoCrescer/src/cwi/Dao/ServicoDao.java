4bb91653ebcea7e9034a875f4667e19b8977fba4
gustavorborba <gustavorobaski@hotmail.com>
2015-11-17 22:29:04 -0200

    Adicionado ServicoDao

--- src/modulo-05-Java/Dia 0-1/ProjetoCrescer/src/cwi/Dao/ServicoDao.java
+++ src/modulo-05-Java/Dia 0-1/ProjetoCrescer/src/cwi/Dao/ServicoDao.java
@@ -1,16 +1,37 @@
 package cwi.Dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import cwi.jdbc.ConnectionFactory;
 import cwi.model.Servico;
 
 public class ServicoDao {
-	public void insert(Servico servico){
+	public void insert(Servico servico) throws SQLException{
 		try(Connection conexao = new ConnectionFactory().getConnection()) {
-			
-		} catch (Exception e) {
-			// TODO: handle exception
+			PreparedStatement statement = conexao.prepareStatement("insert into Servico (idServico,dsServico) values (?,?)");
+			statement.setLong(1, servico.getIDServico());
+			statement.setString(2, servico.getDSServico());
+			statement.execute();
+		} catch (SQLException e) {
+			throw e;
 		}
+	}
+	public List<Servico> listAll() throws SQLException{
+		List<Servico> lista = new ArrayList<Servico>();
+		try(Connection conexao = new ConnectionFactory().getConnection()){
+			PreparedStatement statement = conexao.prepareStatement("select idServico, dsServico from Servico");
+			ResultSet result = statement.executeQuery();
+			while(result.next()){
+				lista.add(new Servico(result.getLong(1),result.getString(2)));
+			}
+		} catch (SQLException e){
+			throw e;
+		}
+		return lista;
 	}
 }
--- src/modulo-05-Java/Dia 0-1/ProjetoCrescer/src/cwi/model/Servico.java
+++ src/modulo-05-Java/Dia 0-1/ProjetoCrescer/src/cwi/model/Servico.java
@@ -4,10 +4,15 @@ public class Servico {
 	private long idServico;
 	private String dsServico;
 	
+	public Servico(long id, String descricao){
+		this.idServico = id;
+		this.dsServico = descricao;
+	}
+	
 	public long getIDServico(){
 		return this.idServico;
 	}
-	public String getdsServico(){
+	public String getDSServico(){
 		return this.dsServico;
 	}
 }
