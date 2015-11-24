7139cd8de7e61dfdef7ce4d04705fdda03d37219
Gustavo Borba <gustavorobaski@hotmail.com>
2015-11-24 17:28:58 -0200

    Refactoring em andamento

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/services/ClienteService.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/services/ClienteService.java
@@ -1,5 +1,20 @@
 package br.com.cwi.crescer.services;
 
-public class ClienteService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import br.com.cwi.crescer.dao.ClienteDao;
+
+@Service
+public class ClienteService {
+	private ClienteDao clienteDAO;
+	
+	@Autowired
+	public ClienteService(ClienteDao dao){
+		this.clienteDAO = dao;
+	}
+	
+	public List<ClienteDTO> listarClientesAtivos(){
+		
+	}
 }
