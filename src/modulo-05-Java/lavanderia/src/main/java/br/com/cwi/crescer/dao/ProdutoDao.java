ada0bbaa769b6a8044084fd9ba9eccbfa3714f84
gustavorborba <gustavorobaski@hotmail.com>
2015-11-27 21:50:41 -0200

    Adicionado listar todos e editar/salvar de Produto

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ProdutoDao.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ProdutoDao.java
@@ -1,10 +1,14 @@
 package br.com.cwi.crescer.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.cwi.crescer.domain.Pedido;
 import br.com.cwi.crescer.domain.Produto;
 
 @Repository
@@ -14,5 +18,19 @@ public class ProdutoDao implements IDao<Produto>{
 	
 	public Produto findById(long id){
 		return em.find(Produto.class, id);
+	}
+	
+	public List<Pedido> listarProdutos(){
+		return em.createQuery("FROM Produto",Pedido.class)
+				.getResultList();
+	}
+	
+	@Transactional
+	public Produto salvarAlteracao(Produto produto){
+		if(produto.idIsNull()){
+			em.persist(produto);
+			return produto;
+		}
+		return em.merge(produto);
 	}
 }
