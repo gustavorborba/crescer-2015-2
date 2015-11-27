03661ba74a1833c901b8840f7624b01569fd7f03
gustavorborba <gustavorobaski@hotmail.com>
2015-11-27 11:06:08 -0200

    Adicionado Update/insert em User

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/UsersDao.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/UsersDao.java
@@ -2,6 +2,7 @@ package br.com.cwi.crescer.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 import br.com.cwi.crescer.domain.Users;
@@ -16,5 +17,15 @@ public class UsersDao {
 		return em.createQuery(" FROM Users u WHERE u.username = :username",Users.class)
 		.setParameter("username", username)
 		.getSingleResult();
+	}
+	
+	@Transactional
+	public Users update(Users user){
+		return em.merge(user);
+	}
+	
+	@Transactional
+	public void salvarNovo(Users user){
+		em.persist(user);
 	}
 }
