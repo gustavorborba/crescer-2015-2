16e422ad7bfc024466a1cbe49d451e9d1796a9ee
gustavorborba <gustavorobaski@hotmail.com>
2015-11-28 15:32:51 -0200

    Corrigido metodos

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ProdutoDao.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ProdutoDao.java
@@ -5,11 +5,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-
 import org.springframework.stereotype.Repository;
-
-import br.com.cwi.crescer.domain.Pedido;
+import br.com.cwi.crescer.domain.Material;
 import br.com.cwi.crescer.domain.Produto;
+import br.com.cwi.crescer.domain.Servico;
 
 @Repository
 public class ProdutoDao{
@@ -20,8 +19,8 @@ public class ProdutoDao{
 		return em.find(Produto.class, id);
 	}
 	
-	public List<Pedido> listarProdutos(){
-		return em.createQuery("FROM Produto",Pedido.class)
+	public List<Produto> listarProdutos(){
+		return em.createQuery("FROM Produto",Produto.class)
 				.getResultList();
 	}
 	
@@ -33,14 +32,14 @@ public class ProdutoDao{
 		}
 		return em.merge(produto);
 	}
-	
-	public Produto buscarCombinacao(Produto produto){
+
+	public List<Produto> buscarCombinacao(Servico servico, Material material){
 		StringBuilder query = new StringBuilder();
 		query.append("FROM Produto p where p.servico = :idServico and p.material = :idMaterial");
 		return em.createQuery(query.toString(),Produto.class)
-			.setParameter("idServico", produto.getServico())
-			.setParameter("idMaterial", produto.getMaterial())
-			.getSingleResult();
-				
+			.setParameter("idServico", servico)
+			.setParameter("idMaterial", material)
+			.getResultList();
+			
 	}
 }
