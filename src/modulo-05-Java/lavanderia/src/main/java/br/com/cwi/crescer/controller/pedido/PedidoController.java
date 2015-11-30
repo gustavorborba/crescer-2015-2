6dad16b63a7a20d1194fa6a8712502fbbf3639c5
gustavorborba <gustavorobaski@hotmail.com>
2015-11-30 01:51:08 -0200

    Refatorado controller

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/controller/pedido/PedidoController.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/controller/pedido/PedidoController.java
@@ -10,48 +10,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.cwi.crescer.domain.Servico;
-import br.com.cwi.crescer.dto.ClienteDTO;
 import br.com.cwi.crescer.dto.TramiteDTO;
-import br.com.cwi.crescer.services.ClienteService;
 import br.com.cwi.crescer.services.ItemService;
-import br.com.cwi.crescer.services.PedidoService;
 import br.com.cwi.crescer.services.ProdutoService;
 import br.com.cwi.crescer.services.ServicoService;
 
 @Controller
 @RequestMapping("/pedido/cadastro")
-public class PedidoCadastroController {
-	private ClienteService clienteService;
+public class PedidoController {
 	private ServicoService servicoService;
 	private ProdutoService produtoService;
-	private PedidoService pedidoService;
 	private ItemService itemService;
 	@Autowired
-	public PedidoCadastroController(ClienteService clienteService, ServicoService servico,
-			ProdutoService produto, PedidoService pedidoService,ItemService item) {
-		this.clienteService = clienteService;
+	public PedidoController(ServicoService servico,
+			ProdutoService produto,ItemService item) {
 		this.servicoService = servico;
 		this.produtoService = produto;
-		this.pedidoService = pedidoService;
 		this.itemService = item;
 	}
 
 	@RequestMapping(path = "/clientecadastro", method = RequestMethod.GET)
 	public ModelAndView cadastro() {
 		return new ModelAndView("/pedido/cadastro/clienteCadastro");
-	}
-
-	@RequestMapping(path = "/clientecadastro", method = RequestMethod.POST)
-	public ModelAndView validarCliente(String cpf, final RedirectAttributes redirectAttributes) {
-		ClienteDTO dto = clienteService.buscarPorCpf(cpf);
-		boolean clienteNaoEncontrado = dto == null;
-		if (clienteNaoEncontrado) {
-			redirectAttributes.addFlashAttribute("Mensagem", "Cliente não encontrado, tente novamente");
-			return new ModelAndView("redirect:/pedido/cadastro/clientecadastro");
-		}
-		return new ModelAndView("/pedido/pedidos", "pedidos", pedidoService.criarDTOQueContemListaDePedidos(dto));
 	}
 	
 	@RequestMapping(path = "/itemcadastro/{idPedido}/{idProduto}/{peso}", method = RequestMethod.GET)
