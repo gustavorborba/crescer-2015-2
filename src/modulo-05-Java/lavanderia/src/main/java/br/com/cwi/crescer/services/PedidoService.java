32fff862b311cd36d75a359c90ef752b0f0c22d7
gustavorborba <gustavorobaski@hotmail.com>
2015-11-30 00:25:52 -0200

    Feat -> adicionado Service a pedido

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/services/PedidoService.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/services/PedidoService.java
@@ -1,9 +1,22 @@
 package br.com.cwi.crescer.services;
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.cwi.crescer.dao.PedidoDao;
+import br.com.cwi.crescer.domain.Cliente;
+import br.com.cwi.crescer.domain.Pedido;
+import br.com.cwi.crescer.domain.Pedido.SituacaoPedido;
+import br.com.cwi.crescer.dto.ClienteDTO;
+import br.com.cwi.crescer.dto.ItemDTO;
+import br.com.cwi.crescer.dto.PedidoClienteDTO;
+import br.com.cwi.crescer.dto.PedidoDTO;
+import br.com.cwi.crescer.mapper.ItemMapper;
+import br.com.cwi.crescer.mapper.PedidoMapper;
 
 @Service
 public class PedidoService {
@@ -12,5 +25,38 @@ public class PedidoService {
 	@Autowired
 	public PedidoService(PedidoDao pedidoDAO){
 		this.pedidoDAO = pedidoDAO;
+	}
+	
+	public void cadastrarPedido(Long idCliente){
+		Pedido pedido = new Pedido();
+		Cliente cliente = new Cliente();
+		cliente.setIdCliente((long)idCliente);
+		pedido.setCliente(cliente);
+		pedido.setDataInclusao(new Date());
+		pedido.setValor(new BigDecimal(0));
+		pedido.setSituacao(SituacaoPedido.PENDENTE);
+		pedidoDAO.cadastrarPedido(pedido);
+	}
+	public List<PedidoDTO> listarPedidosPorCliente(Long idCliente){
+		Cliente cliente = new Cliente();
+		cliente.setIdCliente(idCliente);
+		return PedidoMapper.AllPedidosToDTO(pedidoDAO.listarPedidosPorCliente(cliente));
+	}
+	public List<PedidoDTO> listarPedidos(){
+		
+		return PedidoMapper.AllPedidosToDTO(pedidoDAO.listarPedidos());
+	}
+	public PedidoClienteDTO criarDTOQueContemListaDePedidos(ClienteDTO dto){
+		PedidoClienteDTO pedidoDTO = new PedidoClienteDTO();
+		pedidoDTO.setIdCliente(dto.getId());
+		pedidoDTO.setNomeCliente(dto.getNome());
+		pedidoDTO.setCpf(dto.getCpf());
+		pedidoDTO.setPedidosVInculados(this.listarPedidosPorCliente(dto.getId()));
+		return pedidoDTO;
+	}
+	
+	public List<ItemDTO> listarItensVinculadosAPedido(Long id){
+		Pedido pedido = pedidoDAO.findById(id);
+		return ItemMapper.allItemTOItemDTO(pedido.getItens());
 	}
 }
