b28ec4551c4dbbda2f891cb3ccd247f401ca0890
gustavorborba <gustavorobaski@hotmail.com>
2015-11-30 01:51:42 -0200

    Adicionado metodo de validação para itens processados de determinado Pedido

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ItemDao.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ItemDao.java
@@ -1,5 +1,7 @@
 package br.com.cwi.crescer.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -7,6 +9,8 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 import br.com.cwi.crescer.domain.Item;
+import br.com.cwi.crescer.domain.Item.SituacaoItem;
+import br.com.cwi.crescer.domain.Pedido;
 
 @Repository
 public class ItemDao{
@@ -24,5 +28,11 @@ public class ItemDao{
 			return item;
 		}
 		return em.merge(item);
+	}
+	public List<Item> verificarItemASerProcessado(Pedido pedido){
+		return em.createQuery("From Item i where i.pedido = :pedido and i.situacao = :situacao",Item.class)
+				.setParameter("pedido", pedido)
+				.setParameter("situacao", SituacaoItem.PROCESSADO)
+				.getResultList();
 	}
 }
