35f99e4b7072d09e2caa5db6dd52e88a10307de5
gustavorborba <gustavorobaski@hotmail.com>
2015-11-30 01:50:47 -0200

    Refatorad ocontrolelr

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/controller/pedido/ClientePedidoController.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/controller/pedido/ClientePedidoController.java
@@ -1,32 +1,38 @@
 package br.com.cwi.crescer.controller.pedido;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import br.com.cwi.crescer.dto.TramiteDTO;
-import br.com.cwi.crescer.services.ItemService;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import br.com.cwi.crescer.dto.ClienteDTO;
+import br.com.cwi.crescer.services.ClienteService;
 import br.com.cwi.crescer.services.PedidoService;
 
 @Controller
 @RequestMapping("/pedido")
 public class ClientePedidoController {
 	private PedidoService pedidoService;
-	private ItemService itemService;
+	private ClienteService clienteService;
 	@Autowired
-	public ClientePedidoController(PedidoService pedidoService, ItemService item){
+	public ClientePedidoController(PedidoService pedidoService, ClienteService cliente){
 		this.pedidoService = pedidoService;
-		this.itemService = item;
+		this.clienteService = cliente;
 	}
 	
 	@RequestMapping(path = "/pedidoindex", method = RequestMethod.GET)
 	public ModelAndView cadastro() {
 		return new ModelAndView("/pedido/pedidoindex");
 	}
-	
+	@RequestMapping(path = "/clientepedidos", method = RequestMethod.POST)
+	public ModelAndView validarCliente(String cpf, final RedirectAttributes redirectAttributes) {
+		ClienteDTO dto = clienteService.buscarPorCpf(cpf);
+		boolean clienteNaoEncontrado = dto == null;
+		if (clienteNaoEncontrado) {
+			redirectAttributes.addFlashAttribute("Mensagem", "Cliente não encontrado, tente novamente");
+			return new ModelAndView("redirect:/pedido/cadastro/clientecadastro");
+		}
+		return new ModelAndView("/pedido/pedidos", "pedidos", pedidoService.criarDTOQueContemListaDePedidos(dto));
+	}
 }
