5f04ca84cebfdcf0741cb465fded8f939d2d8bfa
gustavorborba <gustavorobaski@hotmail.com>
2015-11-30 00:22:32 -0200

    Feat -> metodos base

--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ItemDao.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ItemDao.java
@@ -2,16 +2,27 @@ package br.com.cwi.crescer.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.cwi.crescer.domain.Item;
 
 @Repository
-public class ItemDao implements IDao<Item> {
+public class ItemDao{
 	@PersistenceContext
 	private EntityManager em;
 	
 	public Item findById(long id){
 		return em.find(Item.class, id);
+	}
+	
+	@Transactional
+	public Item salvarAlteracoes(Item item){
+		if(item.getIdItem() == null){
+			em.persist(item);
+			return item;
+		}
+		return em.merge(item);
 	}
 }
--- src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/PedidoDao.java
+++ src/modulo-05-Java/lavanderia/src/main/java/br/com/cwi/crescer/dao/PedidoDao.java
@@ -1,18 +1,38 @@
 package br.com.cwi.crescer.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.cwi.crescer.domain.Cliente;
 import br.com.cwi.crescer.domain.Pedido;
 
 @Repository
-public class PedidoDao implements IDao<Pedido>{
+public class PedidoDao{
 	@PersistenceContext
 	private EntityManager em;
 	
 	public Pedido findById(long id){
 		return em.find(Pedido.class, id);
+	}
+	
+	@Transactional
+	public void cadastrarPedido(Pedido pedido){
+		 em.persist(pedido);
+	}
+	
+	public List<Pedido> listarPedidos(){
+		return em.createQuery("FROM Pedido p where p.cliente = :cliente" ,Pedido.class)
+		.getResultList();
+	}
+	
+	public List<Pedido> listarPedidosPorCliente(Cliente cliente){
+		return em.createQuery("FROM Pedido p where p.cliente = :cliente" ,Pedido.class)
+				.setParameter("cliente", cliente)
+				.getResultList();
 	}
 }
